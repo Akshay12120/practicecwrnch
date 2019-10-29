b4e6da645e975f0199f7b115322a28dd0a55c99a
Akshay <Akshay96appknit@gmail.com>
2019-10-29 18:36:02 +0530

    choose region screen updated

--- app/src/main/java/com/app_knit/base/views/fragments/ChooseRegion.kt
+++ app/src/main/java/com/app_knit/base/views/fragments/ChooseRegion.kt
@@ -1,12 +1,10 @@
 package com.app_knit.base.views.fragments
 
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.app_knit.base.R
 import com.app_knit.base.viewmodels.BaseViewModel
 import com.app_knit.base.views.activities.BaseAppCompactActivity
@@ -17,17 +15,38 @@ import kotlinx.android.synthetic.main.toolbar.*
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRegion :BaseFragment() {
+class ChooseRegion : BaseRecyclerViewFragment() {
+
     override val layoutId: Int
         get() = R.layout.fragment_choose_region
+
     override val viewModel: BaseViewModel?
         get() = null
 
-    override fun init() {
-tvToolbarTitle.text="Choose Region"
+    override fun setData() {
+
+        tvToolbarTitle.text = "Choose Region"
         ivToolbarActionEnd.setImageResource(R.drawable.ic_search)
         ivToolbarActionEnd.visibility = (View.VISIBLE);
-        continubtn2.setOnClickListener { (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId = R.id.flFragContainerMain, fragment = Welcome(),isAddFragment = false) }
+        continubtn2.setOnClickListener { (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId = R.id.flFragContainerMain, fragment = Welcome(), isAddFragment = false) }
+        val data = PostSource.createDataset1()
+        Regionrecycleradapter.submitList(data)
+
+    }
+
+    private val Regionrecycleradapter by lazy { RegionRecyclerAdapter(requireContext()) }
+
+    override val recyclerViewAdapter: RecyclerView.Adapter<*>?
+        get() = Regionrecycleradapter
+
+    override val layoutManager: RecyclerView.LayoutManager?
+        get() = GridLayoutManager(context, 2, GridLayoutManager.HORIZONTAL, false)
+    //get() = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+    override val isShowRecyclerViewDivider: Boolean
+        get() = true
+
+    override fun onPullDownToRefresh() {
+
     }
 
 
